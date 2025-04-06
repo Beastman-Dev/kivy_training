d3c7f0a903a7090346aabc2a30b15fa350a0c031
Beastman-Dev <wmeastman@gmail.com>
2025-04-06 11:58:07 -0400

    Completed Sizing and Positioning Pert 3 with wrap-up notes

--- main.py
+++ main.py
@@ -3,22 +3,17 @@ from kivy.uix.button import Button
 
 
 class FunkyButton(Button):
-    def __init__(self, **kwargs):
-        super(FunkyButton, self).__init__(**kwargs)
-        self.text="Funky Button"
-        self.pos=(100,100)
-        self.size_hint=(0.25, 0.25)
+    pass
 
 
 
 class LanguageLearnerApp(App):
     def build(self):
-        # return FunkyButton(
-        #     text="Funky Button",
-        #     pos=(100,100),
-        #     size_hint=(0.5, 0.5)
-        # )
-        return FunkyButton()
-    
+        return FunkyButton(
+            pos=(100,100),
+            size_hint=(None,None),
+            size=(200,200)
+        )
+
 if __name__ == "__main__":
     LanguageLearnerApp().run()
