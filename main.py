069032b59eccb92d7432fe10546202977fcca04e
Beastman-Dev <wmeastman@gmail.com>
2025-04-06 11:16:36 -0400

    Completed Sizing and Positioning Part 1

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from kivy.uix.button import Button
 
 class LanguageLearnerApp(App):
     def build(self):
-        return Button(text="Hello World")
+        return Button(
+            text="Hello World",
+            pos=(50,50),
+            size_hint=(0.8,0.8)
+            )
     
 if __name__ == "__main__":
     LanguageLearnerApp().run()
